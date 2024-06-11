5c7e369fb57c4d11712c38fc54f1b9ee8f203648
SzaboMario <szabo.mario.93@gmail.com>
2024-06-11 19:33:46 +0200

    Add OperationMode enum

---
+++ WorkTableSimulator/OperationMode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkTableSimulator
+{
+    public enum OperationMode
+    {
+        Stopped,
+        Idle,
+        Running,
+        OnError,
+        OnEStop,
+    }
+}
